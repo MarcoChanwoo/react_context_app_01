4c9f39c92acb53a6877347480f315a0dabeb213d
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-12 13:11:37 +0900

    updated

--- src/pages/SummaryPage/index.js
+++ src/pages/SummaryPage/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const SummaryPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <input type="checkbox" checked={false} id="confirm-checkbox" />{" "}
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button type="submit">주문 확인</button>
+      </form>
+    </div>
+  );
 };
 
 export default SummaryPage;
