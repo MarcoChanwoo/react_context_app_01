500c7ca624a85ea18280f80d9587aa3e67dd987e
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-14 17:36:14 +0900

    'updated'

--- src/context/OrderContext.js
+++ src/context/OrderContext.js
@@ -1,4 +1,4 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 
 const OrderContext = createContext();
 
@@ -7,6 +7,37 @@ export function OrderContextProvider(props) {
     products: new Map(),
     options: new Map(),
   });
+
+  const [totals, setTotals] = useState({
+    products: 0,
+    options: 0,
+    total: 0,
+  });
+
+  const pricePerItem = {
+    products: 1000,
+    options: 500,
+  };
+
+  const calculateSubtotal = (orderType, orderCounts) => {
+    let optionCount = 0;
+    for (const count of orderCounts[orderType].values()) {
+      optionCount += count;
+    }
+
+    return optionCount * pricePerItem[orderType];
+  };
+
+  useEffect(() => {
+    const productsTotal = calculateSubtotal("products", orderCounts);
+    const optionsTotal = calculateSubtotal("options", orderCounts);
+    const total = productsTotal + optionsTotal;
+    setTotals({
+      products: productsTotal,
+      options: optionsTotal,
+      total,
+    });
+  }, [orderCounts]);
 
   const value = useMemo(() => {
     function updateItemCount(itemName, newItemCount, orderType) {
