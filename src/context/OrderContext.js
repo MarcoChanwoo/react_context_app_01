c7467f41954405eb828d96cd5a1a2d50029a8e70
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-14 17:19:56 +0900

    'updated'

--- src/context/OrderContext.js
+++ src/context/OrderContext.js
@@ -1,7 +1,15 @@
-import { createContext } from "react";
+import { createContext, useMemo, useState } from "react";
 
 const OrderContext = createContext();
 
 export function OrderContextProvider(props) {
-  return <OrderContextProvider value {...props} />;
+  const [orderCounts, setOrderCounts] = useState({
+    products: new Map(),
+    options: new Map(),
+  });
+
+  const value = useMemo(() => {
+    return [{ ...orderCounts }];
+  }, [orderCounts]);
+  return <OrderContextProvider value={value} {...props} />;
 }
