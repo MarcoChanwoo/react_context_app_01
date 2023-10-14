ac534ecd78ad58c2f58d2f619cbdcf9411fe3404
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-14 17:06:22 +0900

    'updated'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import OrderContext from "./context/OrderContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <OrderContext.Provider>
+      <App />
+    </OrderContext.Provider>
   </React.StrictMode>
 );
 
